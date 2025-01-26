d7cd95ca762ffec34db290ebd32b685582fad21d
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-01-26 19:52:26 +0545

    Day-21

---
+++ Day-21/Routers/index.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  res.json({
+    data: `Hello how are you  ${
+      req?.params?.id
+    } with query data ${JSON.stringify(req.query)}`,
+  });
+});
+
+router.post("/", (req, res) => {
+  req.headers["content-type"];
+  console.log(req?.body);
+  res.json({
+    data: "Hello post",
+    receivedData: req.body,
+  });
+});
+module.exports = router;
--- Day-21/index.js
+++ Day-21/index.js
@@ -1,3 +1,14 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
+
+const PORT = Number(process.env.PORT) || 6000;
+// app.use(express.urlencoded({ extended: false }));
+
+const indexRouter = require("./Routers");
+app.use(express.json());
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Application is Running ${PORT}`);
+});
