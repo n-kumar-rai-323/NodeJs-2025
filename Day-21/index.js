76f16ff3beb3bbf8579b7c35911f046bce75f839
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-01-28 13:21:01 +0300

    Day-21

--- Day-21/index.js
+++ Day-21/index.js
@@ -3,15 +3,21 @@ const express = require("express");
 const app = express();
 
 const PORT = Number(process.env.PORT) || 6000;
-
 const indexRouter = require("./Routers");
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.body.Country = "NP";
+  next();
+});
+
+
 app.use("/", indexRouter);
 
 
-app.use((err, req, res, next)=>{
-  const errMsg = err? err.toString() : "Something went wrong"
-  res.status(500).json({data:"", msg:errMsg});
+app.use((err, req, res, next) => {
+  const errMsg = err ? err.toString() : "Something went wrong"
+  res.status(500).json({ data: "", msg: errMsg });
 })
 
 app.listen(PORT, () => {
--- Day-21/modules/users/user.route.js
+++ Day-21/modules/users/user.route.js
@@ -3,7 +3,7 @@ const router = require("express").Router()
 router.get("/", (req, res, next) => {
     try {
         const sum = 1 + "2";
-        if(!isNaN(sum)){
+        if (!isNaN(sum)) {
             throw new Error("Database error")
         }
         res.json({
@@ -12,6 +12,12 @@ router.get("/", (req, res, next) => {
     } catch (e) {
         next(e)
     }
+})
+
+
+router.post("/", (req, res, next) => {
+    console.log(req.body)
+    res.json({ data: "hello from Post users" })
 })
 
 module.exports = router;
