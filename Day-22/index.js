02a506b91f324e16f5da8525e204e7c46dd5c681
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-01-29 12:36:56 +0300

    Day 22

--- Day-22/index.js
+++ Day-22/index.js
@@ -1,14 +1,17 @@
-require("dotenv").config()
-const express =require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const morgan = require('morgan')
+const app = express();
 const PORT = Number(process.env.PORT) || 5800;
+const indexRouter = require("./routes");
 
-
-const indexRouter =require("./routes");
+app.use(morgan('tiny'));
 app.use(express.json());
-app.use(express.static("public"))
-app.use("/", indexRouter)
+app.use(("/resources"),express.static("public"));
 
-app.listen(PORT,()=>{
-console.log(`Application Running ${PORT}`)
+
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+    console.log(`Application Running ${PORT}`);
 })
--- Day-22/modules/users/user.route.js
+++ Day-22/modules/users/user.route.js
@@ -1,7 +1,51 @@
 const router = require("express").Router()
-router.get("/",(req,res)=>{
-    res.json({
-        data:"Hi From Modules Routes..."
-    })
+const multer = require('multer')
+
+
+
+
+
+
+// const upload = multer({ dest: "public/uploads/" });
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "." + file?.originalname.split(".")[1])
+    }
 })
-module.exports=router
+
+const upload = multer({ storage })
+
+
+
+
+router.get("/", (req, res) => {
+    res.json({
+        data: "Hi From Modules Routes..."
+    })
+});
+
+
+
+
+
+
+router.post("/register", upload.single("profilePic"), (req, res, next) => {
+    try {
+        console.log({ pic: req.file });
+        const URL = "http://localhost:6900/resources/uploads/";
+        const myPicture= URL + req?.file?.filename;
+        res.json({
+            data: `User Registered successfully ${myPicture}` 
+        })
+
+    } catch (e) {
+        next(e)
+    }
+})
+
+
+module.exports = router
