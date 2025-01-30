dfdfe50d635d461bacd84368fb092c1ec3eb956e
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-01-30 13:27:25 +0300

    Day 23

--- Day-23/index.js
+++ Day-23/index.js
@@ -2,12 +2,26 @@ require("dotenv").config()
 
 const express = require("express");
 const app = express()
+const mongoose = require("mongoose")
 const PORT = Number(process.env.PORT) || 5800;
 
 const indexRoute = require("./routes")
+
+mongoose.connect("mongodb://localhost:27017/hotel-mgmt").then(()=>{
+    console.log("Database is connected")
+})
+.catch((e)=>{
+    console.log("Database error")
+})
 app.use(express.json());
 app.use(("/"),express.static("public"));
 app.use("/", indexRoute)
+
+
+app.use((err,req,res,next)=>{
+    const errMsg = err ? err.toString() : "Something went wrong!";
+    res.status(500).json({data:"", msg:errMsg})
+})
 
 app.listen(PORT,()=>{
     console.log(`Application Running ${PORT}`)
--- Day-23/modules/users/user.route.js
+++ Day-23/modules/users/user.route.js
@@ -1,8 +1,5 @@
 const router = require("express").Router()
 const multer = require('multer')
-// const upload = multer({ dest: 'uploads/' })
-
-
 
 
 const storage = multer.diskStorage({
@@ -18,20 +15,44 @@ const upload = multer({ storage })
 
 
 
+const checkRole = (sysRoles = []) => {
+    return (req, res, next) => {
+        try {
+            const { roles: userRole = [] } = req.headers;
+            console.log(userRole)
+            const isValidRole = sysRoles.some((role) => userRole.includes(role));
+            if (!isValidRole) throw new Error("User unauthorized");
+            next();
+        } catch (e) {
+            next(e)
+        }
+    }
+}
 
-
-router.get("/", (req, res, next) => {
+//Read all data 
+router.get("/", checkRole(["admin"]), (req, res, next) => {
     try {
-        res.json({
-            data: "From module user routes"
-        });
+        res.json({ data: `I have 2 number of users` });
     } catch (e) {
         next(e);
     }
+});
 
-})
 
-router.post("/profile", upload.single("ProfilePic"), (req, res, next) => {
+
+
+// router.get("/", (req, res, next) => {
+//     try {
+//         res.json({
+//             data: "From module user routes"
+//         });
+//     } catch (e) {
+//         next(e);
+//     }
+
+// })
+
+router.post("/profile",checkRole(["admin"]), upload.single("ProfilePic"), (req, res, next) => {
     try {
         console.log({ pic: req.file })
         const URL = "http://localhost:6800/uploads/";
