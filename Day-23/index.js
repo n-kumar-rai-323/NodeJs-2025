3f56a0d38409105b987c7dd3d2eec3cbd63cbd3c
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-03-20 09:27:09 +0300

    Day 24

--- Day-23/index.js
+++ Day-23/index.js
@@ -7,22 +7,22 @@ const PORT = Number(process.env.PORT) || 5800;
 
 const indexRoute = require("./routes")
 
-mongoose.connect("mongodb://localhost:27017/hotel-mgmt").then(()=>{
+mongoose.connect("mongodb://localhost:27017/hotel-mgmt").then(() => {
     console.log("Database is connected")
 })
-.catch((e)=>{
-    console.log("Database error")
-})
+    .catch((e) => {
+        console.log("Database error")
+    })
 app.use(express.json());
-app.use(("/"),express.static("public"));
+app.use(("/"), express.static("public"));
 app.use("/", indexRoute)
 
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     const errMsg = err ? err.toString() : "Something went wrong!";
-    res.status(500).json({data:"", msg:errMsg})
+    res.status(500).json({ data: "", msg: errMsg })
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Application Running ${PORT}`)
 })
--- Day-24/index.js
+++ Day-24/index.js
@@ -1,2 +1,8 @@
-const express= requrie("express")
+require("dotenv").config()
+const express=require("express")
 const app=express()
+const PORT = Number(process.env.PORT ) || 8000
+
+app.listen(PORT,()=>{
+console.log(`Application Running ${PORT}`);
+})
