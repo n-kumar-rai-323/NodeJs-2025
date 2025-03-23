1f2abde1a997c24794ea7aaea4aa095239872ad8
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-03-23 15:59:52 +0300

    Project Start

--- Day-19/Routing/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const PORT = Number(process.env.PORT) || 8000;
-const indexRouter = require("./Routers");
-app.use("/", indexRouter);
-app.listen(PORT, () => {
-  console.log(`Application running ${PORT}`);
-});
